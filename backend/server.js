abdd408ab2cb5ee3a5d65b599efbdc59ca7cf898
marmat001 <markusmatu@Markuss-MacBook-Pro.local>
2021-03-10 15:45:22 +0100

    configured for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const { readdirSync } = require('fs')
+const mongoose = require('mongoose')
 const morgan = require('morgan')
-const connectDB = require('./config/db')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv').config()
+
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		})
+
+		console.log(`MongoDB Connected: ${conn.connection.host}`)
+	} catch (error) {
+		console.error(`Error: ${error.message}`)
+		process.exit(1)
+	}
+}
 
 connectDB()
 
@@ -16,7 +32,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(bodyParser.json({ limit: '3mb' }))
-
 
 readdirSync('./backend/routes').map((r) => app.use('/api', require('./routes/' + r)))
 
